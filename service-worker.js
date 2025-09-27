f998d563de3d84c045de7c663a33972eddd691cd
brijesh131313 <bg8000040@gmail.com>
2025-09-27 16:53:56 +0545

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'smartstore-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'beep.ogg',
+  'icon-192.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
+
